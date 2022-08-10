124eb51f80ae20cf14efb9526366431a4a36cad2
kanghyew0n <2018314001@tukorea.ac.kr>
2022-08-10 23:31:53 +0900

    두번째 컴포넌트🎉

---
+++ components/src/components/Badge.js
@@ -0,0 +1,12 @@
+import CounterButton from "./Button";
+
+const Badge = () => {
+  return (
+    <Badge>
+      dkssud
+      <CounterButton />
+    </Badge>
+  );
+};
+
+export default Badge;
---
+++ components/src/components/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const CounterButton = () => {
+  return <div>CounterButton</div>;
+};
+export default CounterButton;
